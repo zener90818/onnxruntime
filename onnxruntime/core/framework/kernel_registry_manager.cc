717a920b48b68c4ed2d1fb5051384def663bb0fa
Scott McKay <skottmckay@gmail.com>
2020-12-23 18:55:20 +1000

    Fix issue with minimal build.

--- onnxruntime/core/framework/kernel_registry_manager.cc
+++ onnxruntime/core/framework/kernel_registry_manager.cc
@@ -86,14 +86,14 @@ Status KernelRegistryManager::SearchKernelRegistry(const onnxruntime::Node& node
     return Status(ONNXRUNTIME, FAIL, create_error_message("The node is not placed on any Execution Provider. "));
   }
 
-#if !defined(ORT_MINIMAL_BUILD) || defined(ORT_EXTENDED_MINIMAL_BUILD)
+  // #if !defined(ORT_MINIMAL_BUILD) || defined(ORT_EXTENDED_MINIMAL_BUILD)
   for (auto& registry : custom_kernel_registries_) {
     status = registry->TryFindKernel(node, std::string(), kernel_def_hash, kernel_create_info);
     if (status.IsOK()) {
       return status;
     }
   }
-#endif
+  //#endif
 
   KernelRegistry* p = nullptr;
   auto iter = provider_type_to_registry_.find(ptype);
