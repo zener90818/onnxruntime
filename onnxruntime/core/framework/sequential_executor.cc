762a81cb33f0d7f3d2ce510028523cbb4ff4080f
suffian khan <sukha@microsoft.com>
2021-03-09 08:31:29 -0800

    fix error on compile

--- onnxruntime/core/framework/sequential_executor.cc
+++ onnxruntime/core/framework/sequential_executor.cc
@@ -101,7 +101,7 @@ static void CalculateTotalInputSizes(const OpKernelContextInternal* op_kernel_co
 #if defined(TRACE_EXECUTION)
       const TensorShape& tensor_shape = p_tensor->Shape();
       size_t element_size = p_tensor->DataType()->Size();
-      LOGS(logger, INFO) << node_name << " input[" << i << "]"
+      std::cout << node_name << " input[" << i << "]"
                          << " is_param=" << is_param
                          << " size=" << tensor_size
                          << " shape=" << tensor_shape.ToString()
